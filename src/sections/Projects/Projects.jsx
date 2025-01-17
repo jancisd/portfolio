98f588681c95f7a665cfd97877904e8f3e6352e4
jancisd <jancisyu@ualberta.ca>
2025-01-17 16:01:41 -0700

    Updates

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,14 +2,15 @@
 import styles from './ProjectsStyles.module.css';
 import EventWizard from '../../assets/eventwizard.png'
 import ProjectCard from '../../common/ProjectCard';
+import AntiSleepDetection from '../../assets/anti-sleep.png'
 
 
 function Projects() {
     return <section id="projects"className={styles.container}>
         <h1 className="sectionTitle">Projects</h1>
         <div className={styles.projectsContainer}>
-            <ProjectCard src={EventWizard} link="https://github.com/wizard-cmput301/wizard-project" h3="EventWizard" p="Event Registration App"/>
-
+            <ProjectCard src={EventWizard} link="https://github.com/wizard-cmput301/wizard-project" h3="Event Wizard" p="Event Registration App"/>
+            <ProjectCard src={AntiSleepDetection} link="https://github.com/jancisd/AntiSleepDetection" h3="Anti-Sleep Detection" p="Drowsiness Detection System"/>
         </div>
 
     </section>
